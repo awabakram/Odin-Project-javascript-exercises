60897a3e95fbc1e3f78113ce484878cb97428e16
awabakram <awabakram@live.com>
2023-08-07 13:48:41 +0200

    Did exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, noOfTimes) {
+    if(noOfTimes < 0)
+    {
+        return "ERROR";
+    }
+    let newString = "";
+    for(let i = 0; i < noOfTimes; i++)
+    {
+        newString += inputString;
+    }
+    return newString;
 };
 
 // Do not edit below this line
